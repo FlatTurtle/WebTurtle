78035783606d3049ddb73a8bc83fe182bb42f681
Jens Segers <segers.jens@gmail.com>
2013-11-01 18:25:08 +0100

    Adding Villo popup

--- turtles/villo/dna.js
+++ turtles/villo/dna.js
@@ -5,6 +5,46 @@
     {
         initialize : function(models, options)
         {
+            this.options = options;
+
+            // immediately get collection data
+            this.fetch();
+        },
+        url : function()
+        {
+            var location = this.options.location.split(",");
+
+            return "https://data.irail.be/Bikes/Villo.json?lat=" + encodeURIComponent(location[0]) + "&long=" + encodeURIComponent(location[1]) + "&offset=0&rowcount=1";
+        },
+        parse : function(json)
+        {
+            var villo = json.Villo;
+
+            for (var i in villo)
+            {
+                if (villo[i].distance)
+                {
+                    villo[i].distance = Math.round(parseInt(villo[i].distance)/10)*10;
+                }
+
+                var name = jQuery.trim(villo[i].name);
+                name = name.match(/^[0-9]+\s*-\s*(.*?)(?:[\/|:](.*))?$/)[1];
+                villo[i].name = name.capitalize();
+
+                if (!villo[i].freebikes)
+                {
+                    villo[i].freebikes = 0;
+                }
+
+                if (!villo[i].freespots)
+                {
+                    villo[i].freespots = 0;
+                }
+
+                villo[i].freespots += villo[i].freebikes;
+            }
+
+            return villo;
         }
     });
 
@@ -12,22 +52,50 @@
     {
         initialize : function(options)
         {
+            // prevents loss of 'this' inside methods
+            _.bindAll(this, "render", "popup");
+
             this.options = options;
+            var self = this;
 
+            // get template
+            $.get("turtles/villo/view.html", function(template)
+            {
+                self.template = template;
+
+                // check if we're ready to add the popup
+                self.popup();
+            });
+
+            // split longitude and latitude
             var location = options.location.split(",");
-
             this.options.latitude = location[0];
             this.options.longitude = location[1];
 
             this.render();
+
+            // show popup when collection is ready
+            this.collection.on("sync", this.popup);
         },
         render : function()
         {
             // add marker
-            var marker = Map.marker(this.options.latitude, this.options.longitude, "images/bike.png");
+            this.marker = Map.marker(this.options.latitude, this.options.longitude, "images/bike.png");
+        },
+        popup : function()
+        {
+            // wait for everything to load
+            if (!this.template) return;
+            if (!this.collection.length) return;
+            if (!this.marker) return;
+
+            var data = this.collection.toJSON()[0];
+
+            // render view
+            var html = Mustache.render(this.template, data);
 
             // add popup
-            var popup = Map.popup(marker, "<strong>Villo</strong>");
+            var popup = Map.popup(this.marker, html);
         }
     });
 
