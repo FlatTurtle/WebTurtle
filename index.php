21e4da6e244fd9177cdb08aa9701dcd9d9b23b95
Jens Segers <segers.jens@gmail.com>
2014-04-14 21:34:35 +0200

    Cache breaker

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta charset="utf-8">
     <title></title>
-    <link href="dist/css/style.css?v=1104" rel="stylesheet" type="text/css" />
+    <link href="dist/css/style.css?v=1404" rel="stylesheet" type="text/css" />
     <link href="//fast.fonts.com/cssapi/66253153-9c89-413c-814d-60d3ba0d6ac2.css" type="text/css" >
     <link href="img/favicon.ico" rel="icon" type="image/x-icon">
 </head>
 <body>
 
-    <div id="logo"><a href="https://flatturtle.com" target="_blank"><img src="img/logo.svg?v=1104"></a></div>
+    <div id="logo"><a href="https://flatturtle.com" target="_blank"><img src="img/logo.svg"></a></div>
     <div id="legend">
         <ul class="legend text-color">
             <li class="green">&gt; 80 km/h&nbsp;&nbsp;—</li>
@@ -24,11 +24,11 @@
     <div id="map-canvas"></div>
 
     <script src="//maps.googleapis.com/maps/api/js?sensor=false"></script>
-    <script src="dist/js/vendor.js"></script>
+    <script src="dist/js/vendor.js?v=1404"></script>
     <!--[if lte IE 9]>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-ajaxtransport-xdomainrequest/1.0.1/jquery.xdomainrequest.min.js"></script>
     <![endif]-->
-    <script src="dist/js/flatturtle.js?v=1104"></script>
+    <script src="dist/js/flatturtle.js?v=1404"></script>
 
     <script>
     $(document).ready(function() {
